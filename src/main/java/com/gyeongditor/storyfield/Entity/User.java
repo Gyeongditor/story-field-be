f5631236c718616843468ad8ed49d778ddc471f9
성연준 <elwlahs60@gmail.com>
2025-06-21 15:45:34 +0900

    Add User, Story, StoryPage Entity

---
+++ src/main/java/com/gyeongditor/storyfield/Entity/User.java
@@ -0,0 +1,39 @@
+package com.gyeongditor.storyfield.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @Column(name = "user_id", length = 36)
+    private String userId;
+
+    @Column(name = "login_id", length = 36, nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "user_pw", length = 60, nullable = false)
+    private String userPw;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    private Integer age;
+
+    @Column(name = "sex", length = 10)
+    private String sex; // 추후 Enum 처리 가능
+
+    @Column(name = "user_email", length = 255)
+    private String userEmail;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+}
