f9db02e2f04ec9253282dd3f280a4e8b51a24404
승일 <seoung1515@naver.com>
2025-06-21 14:57:36 +0900

    Initial: 프로젝트 게시

---
+++ src/main/java/com/gyeongditor/storyfield/StoryfieldApplication.java
@@ -0,0 +1,13 @@
+package com.gyeongditor.storyfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryfieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyeongditor/storyfield/StoryfieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyeongditor.storyfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryfieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
