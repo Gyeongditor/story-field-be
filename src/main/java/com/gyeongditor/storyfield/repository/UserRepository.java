4527377e90b31da9b9c42d69fab4cd4cef06274a
SeungIlB <piseouk787@gmail.com>
2025-06-25 16:53:50 +0900

    Feat: 사용자 회원가입 기능

--- src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
+++ src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.gyeongditor.storyfield.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByLoginId(String loginId);
 }
