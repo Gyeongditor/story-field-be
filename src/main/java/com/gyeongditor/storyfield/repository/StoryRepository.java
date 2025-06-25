b8133e5b22253951d93d20ac1afd89e83c897506
SeungIlB <piseouk787@gmail.com>
2025-06-26 02:45:14 +0900

    Add: Jpa 레포지토리 생성

--- src/main/java/com/gyeongditor/storyfield/repository/StoryRepository.java
+++ src/main/java/com/gyeongditor/storyfield/repository/StoryRepository.java
@@ -2,9 +2,8 @@ package com.gyeongditor.storyfield.repository;
 
 import com.gyeongditor.storyfield.Entity.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StoryRepository extends JpaRepository<Story, String> {
-    public boolean existsById(String storyId);
+import java.util.UUID;
+
+public interface StoryRepository extends JpaRepository<Story, UUID> {
 }
--- src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
+++ src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.gyeongditor.storyfield.repository;
 import com.gyeongditor.storyfield.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByLoginId(String loginId);
 }
