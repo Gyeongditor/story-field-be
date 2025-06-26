ed742c3e0ce99a976d79a4b38f11d9793d1d778b
SeungIlB <piseouk787@gmail.com>
2025-06-26 14:34:11 +0900

    Feat: JPA 페이징으로 조회 메서드

--- src/main/java/com/gyeongditor/storyfield/repository/StoryRepository.java
+++ src/main/java/com/gyeongditor/storyfield/repository/StoryRepository.java
@@ -1,9 +1,13 @@
 package com.gyeongditor.storyfield.repository;
 
 import com.gyeongditor.storyfield.Entity.Story;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface StoryRepository extends JpaRepository<Story, UUID> {
+    Page<Story> findAll(Pageable pageable);
+
 }
