3c6cf5dfb0579cc2560ee186b4a8a48c3a6e4504
성연준 <elwlahs60@gmail.com>
2025-06-26 18:51:40 +0900

    FIXED Repository Error

--- src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
+++ src/main/java/com/gyeongditor/storyfield/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.gyeongditor.storyfield.repository;
 
 import com.gyeongditor.storyfield.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-    boolean existsByLoginId(String loginId);
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByLoginId(String loginId);
 }
