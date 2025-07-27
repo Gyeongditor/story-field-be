635c768f8ff470642073342c9f35bb9f5736c4dc
SeungIlB <piseouk787@gmail.com>
2025-07-28 01:43:08 +0900

    Chore: 패키지명 수정

--- src/main/java/com/gyeongditor/storyfield/service/UserService.java
+++ src/main/java/com/gyeongditor/storyfield/service/UserService.java
@@ -51,7 +51,7 @@ public class UserService {
         userRepository.save(user);
 
         // UserResponseDTO 생성
-        return new seungil.login_boilerplate.dto.UserResponseDTO(user.getUserId(), user.getUserName(), user.getEmail());
+        return new UserResponseDTO(user.getUserId(), user.getUserName(), user.getEmail());
     }
 
     // 이메일 중복 체크 메서드
@@ -72,11 +72,11 @@ public class UserService {
     }
 
     // 이메일 검증
-    public seungil.login_boilerplate.dto.UserResponseDTO verifyEmail(String token) {
+    public UserResponseDTO verifyEmail(String token) {
         User user = findUserByVerificationToken(token);
         user.enableAccount(); // 엔티티 메서드 사용
         userRepository.save(user);
-        return new seungil.login_boilerplate.dto.UserResponseDTO(user.getEmail(), user.getUserName());
+        return new UserResponseDTO(user.getEmail(), user.getUserName());
     }
 
     // 비밀번호 암호화 메서드
@@ -88,7 +88,7 @@ public class UserService {
     public UserResponseDTO getUserById(UUID userId) {
         Optional<User> user = userRepository.findById(userId);
         if (user.isPresent()) {
-            return new seungil.login_boilerplate.dto.UserResponseDTO(user.get().getUserId(), user.get().getEmail(), user.get().getUserName());
+            return new UserResponseDTO(user.get().getUserId(), user.get().getEmail(), user.get().getUserName());
         } else {
             throw new IllegalStateException("사용자를 찾을 수 없습니다.");
         }
@@ -105,7 +105,7 @@ public class UserService {
             sendEmail(user.getEmail(), verificationToken, "회원 정보 수정용 이메일 인증");
 
             userRepository.save(user);
-            return new seungil.login_boilerplate.dto.UserResponseDTO(user.getEmail(), user.getUserName());
+            return new UserResponseDTO(user.getEmail(), user.getUserName());
         }).orElseThrow(() -> new IllegalStateException("사용자를 찾을 수 없습니다."));
     }
 
