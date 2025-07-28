e08407459f6c0951527d1d57f786b4f3ce16ae7e
Mr-Sung98 <elwlahs60@gmail.com>
2025-07-28 16:41:34 +0900

    Feat: 모든 DTO 및 Controller에 Swagger 'Schema' 및 'Operation' 어노테이션 추가

--- src/main/java/com/gyeongditor/storyfield/dto/UserDTO/SignUpDTO.java
+++ src/main/java/com/gyeongditor/storyfield/dto/UserDTO/SignUpDTO.java
@@ -1,5 +1,6 @@
 package com.gyeongditor.storyfield.dto.UserDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import jakarta.validation.constraints.Pattern;
@@ -7,17 +8,21 @@ import jakarta.validation.constraints.Pattern;
 @Getter
 @Setter
 @AllArgsConstructor
+@Schema(description = "회원가입 요청 DTO")
 public class SignUpDTO {
 
+    @Schema(description = "회원가입 이메일 주소", example = "newuser@example.com")
     @NotBlank(message = "이메일을 입력해주세요")
     private final String email;
 
+    @Schema(description = "회원가입 비밀번호", example = "SignUp123!")
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
             message = "비밀번호는 영문 대소문자, 숫자, 특수문자를 포함하여 8~16자여야 합니다")
     private final String password;
 
-    @NotBlank(message = "사용자 이름을 입력해주세요")
+    @Schema(description = "회원 이름", example = "홍길동")
+    @NotBlank(message = "이름을 입력해주세요")
     private final String username;
 
     @Override
