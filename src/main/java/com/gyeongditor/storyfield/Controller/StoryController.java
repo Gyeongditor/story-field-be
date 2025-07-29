8129ba83e2772e1903f24cdf5ec93ad77097ad61
승일 <seoung1515@naver.com>
2025-07-29 17:06:14 +0900

    Refactor: path 수정

--- src/main/java/com/gyeongditor/storyfield/Controller/StoryController.java
+++ src/main/java/com/gyeongditor/storyfield/Controller/StoryController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/story")
+@RequestMapping("/story")
 @RequiredArgsConstructor
 public class StoryController {
 
