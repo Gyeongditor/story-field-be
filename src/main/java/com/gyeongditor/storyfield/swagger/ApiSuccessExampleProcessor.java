7e038bf79621be83c362960ce9bf32a83a3b47b7
SeungIlB <piseouk787@gmail.com>
2025-08-22 21:08:55 +0900

    #87 Feat: Swagger 성공 응답 자동화 기능 추가

---
+++ src/main/java/com/gyeongditor/storyfield/swagger/ApiSuccessExampleProcessor.java
@@ -0,0 +1,53 @@
+package com.gyeongditor.storyfield.swagger;
+
+import com.gyeongditor.storyfield.response.SuccessCode;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+
+@Component
+public class ApiSuccessExampleProcessor implements OperationCustomizer {
+
+    @Override
+    public io.swagger.v3.oas.models.Operation customize(io.swagger.v3.oas.models.Operation operation, HandlerMethod handlerMethod) {
+        ApiSuccessResponse annotation = handlerMethod.getMethodAnnotation(ApiSuccessResponse.class);
+        if (annotation != null) {
+            for (SuccessCode successCode : annotation.value()) {
+                // ✅ 응답 스키마 + 예제 동시 등록
+                ApiResponse response = new ApiResponse()
+                        .description(successCode.getMessage())
+                        .content(new Content().addMediaType("application/json",
+                                new MediaType()
+                                        .schema(new Schema<>().type("object")) // <- 스키마 명시
+                                        .addExamples(successCode.name(),
+                                                new Example().value(buildExample(successCode)))
+                        ));
+
+                operation.getResponses().addApiResponse(
+                        String.valueOf(successCode.getStatus().value()), response
+                );
+            }
+        }
+        return operation;
+    }
+
+    private String buildExample(SuccessCode successCode) {
+        return """
+               {
+                 "status": %d,
+                 "code": "%s",
+                 "message": "%s",
+                 "data": null
+               }
+               """.formatted(
+                successCode.getStatus().value(),
+                successCode.getCode(),
+                successCode.getMessage()
+        );
+    }
+}
