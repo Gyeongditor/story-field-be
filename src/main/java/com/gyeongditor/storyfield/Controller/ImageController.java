f415ba3707d5c5e08819817fa00ba1a478ac3af5
성연준 <elwlahs60@gmail.com>
2025-10-21 15:15:59 +0900

    #156 Refactor: ImageController에 이미지 업로드 추가

--- src/main/java/com/gyeongditor/storyfield/Controller/ImageController.java
+++ src/main/java/com/gyeongditor/storyfield/Controller/ImageController.java
@@ -6,12 +6,19 @@ import com.gyeongditor.storyfield.swagger.api.ImageApi;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController implements ImageApi {
 
     private final S3Service s3Service;
+
+    @Override
+    public ApiResponseDTO<List<String>> uploadImageFile(List<MultipartFile> files, HttpServletRequest request) {
+        return s3Service.uploadImageFile(files, request);
+    }
 
     @Override
     public ApiResponseDTO<String> getImageUrl(String fileName, HttpServletRequest request) {
